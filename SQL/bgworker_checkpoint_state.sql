a9a6197f307668a237ae7a99e56bcd6cbb988d80
xiongcccc <xiongcc_1994@126.com>
2023-04-14 11:26:09 +0800

    update sql

--- SQL/bgworker_checkpoint_state.sql
+++ SQL/bgworker_checkpoint_state.sql
@@ -1,12 +1,13 @@
 SELECT
     checkpoints_timed + checkpoints_req AS total_checkpoints,
-    pg_size_pretty(block_size * buffers_clean ) AS bgworker_total_writen,
-    pg_size_pretty(block_size * buffers_checkpoint ) AS chkpointer_total_writen,
-    pg_size_pretty(block_size * buffers_backend ) AS backend_total_writen,
+    pg_size_pretty(block_size * buffers_clean) AS bgworker_total_writen,
+    pg_size_pretty(block_size * buffers_checkpoint) AS chkpointer_total_writen,
+    pg_size_pretty(block_size * buffers_backend) AS backend_total_writen,
     pg_size_pretty(block_size * buffers_checkpoint / (checkpoints_timed + checkpoints_req)) AS checkpoint_write_avg,
     EXTRACT(EPOCH FROM (now() - pg_postmaster_start_time())) / (checkpoints_timed + checkpoints_req) / 60 AS minutes_between_checkpoints,
     buffers_backend_fsync
 FROM
     pg_stat_bgwriter,
     (
-        SELECT current_setting('block_size')::int AS block_size) AS bs
+        SELECT
+            current_setting('block_size')::int AS block_size) AS bs
--- SQL/index_state.sql
+++ SQL/index_state.sql
@@ -1,23 +1,37 @@
 SELECT
-  t.tablename,
-  indexname,
-  c.reltuples AS num_rows,
-  pg_size_pretty(pg_relation_size(quote_ident(t.tablename)::text)) AS table_size,
-  pg_size_pretty(pg_relation_size(quote_ident(indexrelname)::text)) AS index_size,
-  CASE WHEN indisunique THEN 'Y'
-    ELSE 'N'
-  END AS UNIQUE,
-  idx_scan AS number_of_scans,
-  idx_tup_read AS tuples_read,
-  idx_tup_fetch AS tuples_fetched
-FROM pg_tables t
-  LEFT OUTER JOIN pg_class c ON t.tablename=c.relname
-  LEFT OUTER JOIN
-    ( SELECT c.relname AS ctablename, ipg.relname AS indexname, x.indnatts AS number_of_columns, idx_scan, idx_tup_read, idx_tup_fetch, indexrelname, indisunique FROM pg_index x
-      JOIN pg_class c ON c.oid = x.indrelid
-      JOIN pg_class ipg ON ipg.oid = x.indexrelid
-      JOIN pg_stat_all_indexes psai ON x.indexrelid = psai.indexrelid )
-    AS foo
-  ON t.tablename = foo.ctablename
-WHERE t.schemaname='public'
-ORDER BY 1,2;
+    t.tablename,
+    indexname,
+    c.reltuples AS num_rows,
+    pg_size_pretty(pg_relation_size(quote_ident(t.tablename)::text)) AS table_size,
+    pg_size_pretty(pg_relation_size(quote_ident(indexrelname)::text)) AS index_size,
+    CASE WHEN indisunique THEN
+        'Y'
+    ELSE
+        'N'
+    END AS UNIQUE,
+    idx_scan AS number_of_scans,
+    idx_tup_read AS tuples_read,
+    idx_tup_fetch AS tuples_fetched
+FROM
+    pg_tables t
+    LEFT OUTER JOIN pg_class c ON t.tablename = c.relname
+    LEFT OUTER JOIN (
+        SELECT
+            c.relname AS ctablename,
+            ipg.relname AS indexname,
+            x.indnatts AS number_of_columns,
+            idx_scan,
+            idx_tup_read,
+            idx_tup_fetch,
+            indexrelname,
+            indisunique
+        FROM
+            pg_index x
+            JOIN pg_class c ON c.oid = x.indrelid
+            JOIN pg_class ipg ON ipg.oid = x.indexrelid
+            JOIN pg_stat_all_indexes psai ON x.indexrelid = psai.indexrelid) AS foo ON t.tablename = foo.ctablename
+WHERE
+    t.schemaname = 'public'
+ORDER BY
+    1,
+    2;

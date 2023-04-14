d76ddb1fafe9bddfb55745ad0183a23442433abe
xiongcccc <xiongcc_1994@126.com>
2023-04-14 11:11:21 +0800

    update sql index_duplicate

--- SQL/index_duplicate.sql
+++ SQL/index_duplicate.sql
@@ -1,9 +1,22 @@
-SELECT pg_size_pretty(sum(pg_relation_size(idx))::bigint) as size,
-       (array_agg(idx))[1] as idx1, (array_agg(idx))[2] as idx2,
-       (array_agg(idx))[3] as idx3, (array_agg(idx))[4] as idx4
+SELECT
+    sub.table,
+    pg_size_pretty(sum(pg_relation_size(idx))::bigint) AS size,
+    (array_agg(idx))[1] AS idx1,
+    (array_agg(idx))[2] AS idx2,
+    (array_agg(idx))[3] AS idx3,
+    (array_agg(idx))[4] AS idx4
 FROM (
-    SELECT indexrelid::regclass as idx, (indrelid::text ||E'\n'|| indclass::text ||E'\n'|| indkey::text ||E'\n'||
-                                         coalesce(indexprs::text,'')||E'\n' || coalesce(indpred::text,'')) as key
-    FROM pg_index) sub
-GROUP BY key HAVING count(*)>1
-ORDER BY sum(pg_relation_size(idx)) DESC;
+    SELECT
+        s.schemaname || '.' || s.relname AS table,
+        i.indexrelid::regclass AS idx,
+        (i.indrelid::text || E'\n' || i.indclass::text || E'\n' || i.indkey::text || E'\n' || coalesce(i.indexprs::text, '') || E'\n' || coalesce(i.indpred::text, '')) AS key
+    FROM
+        pg_index i
+        JOIN pg_stat_all_indexes s ON i.indexrelid = s.indexrelid) sub
+GROUP BY
+    sub.table,
+    key
+HAVING
+    count(*) > 1
+ORDER BY
+    sum(pg_relation_size(idx)) DESC;

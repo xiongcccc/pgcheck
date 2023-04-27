727337bb90efe6389b163a28ed16bb0e1023adbb
xiongcccc <xiongcc_1994@126.com>
2023-04-27 14:33:47 +0800

    update sql of partition info

--- SQL/partition_inherit_info.sql
+++ SQL/partition_inherit_info.sql
@@ -1,13 +1,15 @@
 SELECT
-     nmsp_parent.nspname AS parent_schema,
-     parent.relname AS parent,
-     nmsp_child.nspname AS child_schema,
-     child.relname AS child
- FROM
-     pg_inherits
-     JOIN pg_class parent ON pg_inherits.inhparent = parent.oid
-     JOIN pg_class child ON pg_inherits.inhrelid = child.oid
-     JOIN pg_namespace nmsp_parent ON nmsp_parent.oid = parent.relnamespace
-     JOIN pg_namespace nmsp_child ON nmsp_child.oid = child.relnamespace
- WHERE
-     child.relkind = 'r';
+    nmsp_parent.nspname AS parent_schema,
+    parent.relname AS parent,
+    nmsp_child.nspname AS child_schema,
+    child.relname AS child
+FROM
+    pg_inherits
+    JOIN pg_class parent ON pg_inherits.inhparent = parent.oid
+    JOIN pg_class child ON pg_inherits.inhrelid = child.oid
+    JOIN pg_namespace nmsp_parent ON nmsp_parent.oid = parent.relnamespace
+    JOIN pg_namespace nmsp_child ON nmsp_child.oid = child.relnamespace
+WHERE
+    child.relkind = 'r'
+ORDER BY
+    child.relname;
